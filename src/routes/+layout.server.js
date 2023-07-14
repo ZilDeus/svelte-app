429ef9c8675bb4b51661318236743fe3b7d763da
ZilDeus <zildeus@protonmail.com>
2023-07-14 17:20:16 +0000

    please work4

--- src/routes/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import {redirect} from '@sveltejs/kit'
-export async function load({ cookies }) {
-    const res = await axios({
-      method:"POST",
-      url:"https://gobackend2-zildeus.b4a.run/start",
-      }).catch(()=>{ redirect(301,"/404")})
-}
